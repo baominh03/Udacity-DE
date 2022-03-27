08a2acf3ca6a431267953579b7fa7f0c03e38b69
baominh <leminhbao2709@gmail.com>
2022-03-27 22:48:52 +0700

    submit proj4

---
+++ project4-data-lake/README.md
@@ -0,0 +1,87 @@
+# Project 4: Data Lake
+In this project, you'll apply what you've learned on Spark and data lakes to build an ETL pipeline for a data lake hosted on S3. To complete the project, you will need to load data from S3, process the data into analytics tables using Spark, and load them back into S3. You'll deploy this Spark process on a cluster using AWS.
+
+# Schema for Song Play Analysis
+
+
+## Fact tables
+### songplay tables
+Primary key: songplay_id
+Foreign key: start_time, user_id, song_id, artist_id
+| Column      | Type        | Nullable | Explanation                                                                                   |
+| ----------- | ----------- | -------- | --------------------------------------------------------------------------------------------- |
+| songplay_id | SERIAL      |          | In Log dataset, there are no songplay_id => songplay_id should auto increment (SERIAL)        |
+| start_time  | TIMESTAMP   | not null | "registration" log dataset                                                                    |
+| user_id     | INT         | not null | "userId" log datase                                                                           |
+| level       | VARCHAR(4)  | not null | "level" log dataset                                                                           |
+| song_id     | VARCHAR(18) | not null | "song"   log dataset =  "title" song dataset => query song_id (limit 18 characters)           |
+| artist_id   | VARCHAR(18) | not null | "artist"  log dataset =  "artist_name" song data set => query artist_id (limit 18 characters) |
+| session_id  | INT         | not null | "sessionId" log dataset                                                                       |
+| location    | VARCHAR     | not null | "location" log dataset                                                                        |
+| user_agent  | VARCHAR     | not null | "userAgent" log dataset                                                                       |
+
+## Dimension tables
+
+### users tables
+Primary key: user_id
+| Column     | Type       | Nullable | Explanation             |
+| ---------- | ---------- | -------- | ----------------------- |
+| user_id    | INT        | not null | "userId" log datase     |
+| first_name | VARCHAR    | not null | "firstName" log dataset |
+| last_name  | VARCHAR    | not null | "lastName" log dataset  |
+| gender     | CHAR(1)    | not null | "gender" log dataset    |
+| level      | VARCHAR(4) | not null | "level" log dataset     |
+
+### songs tables
+Primary key: song_id
+| Column    | Type        | Nullable | Explanation                                  |
+| --------- | ----------- | -------- | -------------------------------------------- |
+| song_id   | VARCHAR(18) | not null | "song_id" song dataset (limit 18 characters) |
+| title     | VARCHAR     | not null | "title" song dataset                         |
+| artist_id | VARCHAR(18) | not null | "artist_id" song dataset                     |
+| year      | INT         | not null | "year" song dataset                          |
+| duration  | FLOAT       | not null | "duration" song dataset                      |
+
+### artists tables
+Primary key: artist_id
+| Column     | Type        | Nullable | Explanation                                    |
+| ---------- | ----------- | -------- | ---------------------------------------------- |
+| artist_id  | VARCHAR(18) | not null | "artist_id" song dataset (limit 18 characters) |
+| name       | VARCHAR     | not null | "artist_name" song dataset                     |
+| location   | VARCHAR     |          | "artist_location" song dataset                 |
+| latitude   | FLOAT       |          | "artist_latitude" song dataset                 |
+| longtitude | FLOAT       |          | "artist_longtitute" song dataset               |
+
+### time tables
+Primary key: start_time
+| Column     | Type      | Nullable | Explanation                                                  |
+| ---------- | --------- | -------- | ------------------------------------------------------------ |
+| start_time | TIMESTAMP | not null | "ts" log dataset                                             |
+| hour       | INT       | not null | break down from start_time                                   |
+| day        | INT       |          | break down from start_time                                   |
+| week       | INT       |          | break down from start_time                                   |
+| month      | INT       |          | break down from start_time                                   |
+| year       | INT       |          | break down from start_time                                   |
+| weekday    | INT       |          | break down from start_time (0 Monday, 1 Tuesday, … 6 Sunday) |
+
+# Explanation of the files in the repository
+1. ```./s3_helper/create_cluster.py```: base on value from dl.cfg to create s3 bucket
+2. ```./s3_helper/delete_cluster.py```: base on value from dl.cfg to delete s3 bucket
+3. ```etl.ipynb```: reads and processes a single file from song_data and log_data and loads the data into our tables. This notebook contains detailed instructions on the ETL process for each of the tables.
+4. ```etl.py```: reads and processes files from song_data and log_data and loads them into s3 output bucket
+5. ```query.ipynb```: To check the result
+6. ```README.md```: provides discussion on our project.
+
+# Environment set up (In case run locally)
+1. Install pandas: ```pip install pandas```
+2. Install pyspark: ```pip install pyspark```
+
+# Run scripts
+
+1. Create/use IAM user with attach policy ```AmazonS3FullAccess```
+2. Manually input AWS  KEY and AWS Secret KEY into ```dwh.cfg``` 
+3. ```python s3_helper/create_s3.py```
+4. ```python etl.py``` wait for loading (around 30 mins)
+5. run  ```query.ipynb``` file to check the result
+6. ```python s3_helper/delete_s3.py```
+
--- project4-data-lake/etl.py
+++ project4-data-lake/etl.py
@@ -1,16 +1,26 @@
+from calendar import weekday
 import configparser
 from datetime import datetime
 import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+from pyspark.sql.functions import monotonically_increasing_id
+import pandas as pd
+from time import time
 
+
+# Run local
+config_file_path = './project4-data-lake/dl.cfg'
+# Run on Udacity workspace
+# config_file_path = 'dl.cfg'
 
 config = configparser.ConfigParser()
-config.read('dl.cfg')
+config.read_file(open(config_file_path))
 
-os.environ['AWS_ACCESS_KEY_ID']=config.get['AWS', 'AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config.get['AWS', 'AWS_SECRET_ACCESS_KEY']
+os.environ['AWS_ACCESS_KEY_ID'] = config.get('AWS','AWS_ACCESS_KEY_ID')
+os.environ['AWS_SECRET_ACCESS_KEY'] = config.get('AWS','AWS_SECRET_ACCESS_KEY')
+S3_BUCKET_OUTPUT = config.get("S3", "S3_BUCKET_OUTPUT")
 
 
 def create_spark_session():
@@ -22,72 +32,105 @@ def create_spark_session():
 
 
 def process_song_data(spark, input_data, output_data):
+    """
+        To read josn file from song_data and create songs_table and artists_table to s3 output bucket
+        
+        INPUT:
+            spark       : from create_spark_session() function
+            input_data  : s3 input bucket
+            output_data : s3 output bucket
+            
+    """
     # get filepath to song data file
-    song_data = 
+    song_data = input_data + 'song_data/*/*/*/*.json'
     
     # read song data file
-    df = 
+    df = spark.read.json(song_data)
 
     # extract columns to create songs table
-    songs_table = 
+    songs_table = df.select("song_id","title","artist_id","year","duration").dropDuplicates()
     
     # write songs table to parquet files partitioned by year and artist
-    songs_table
+    songs_table.write.mode('overwrite').partitionBy("year", "artist_id").parquet(output_data + 'songs_table/')
 
     # extract columns to create artists table
-    artists_table = 
+    artists_table = df.select("artist_id","artist_name", col("artist_location").alias("location"), col("artist_latitude").alias("latitude"), col("artist_longitude").alias("longitude")).dropDuplicates()
+    
     
     # write artists table to parquet files
-    artists_table
+    artists_table.write.mode('overwrite').parquet(output_data + 'artists_table/')
 
 
 def process_log_data(spark, input_data, output_data):
+    """
+        To read json file from log-data and create users_table, time_table and songplays_table to s3 output bucket
+        
+        INPUT:
+            spark       : from create_spark_session() function
+            input_data  : s3 input bucket
+            output_data : s3 output bucket
+            
+    """
     # get filepath to log data file
-    log_data =
+    log_data = input_data + 'log-data/*.json'
 
     # read log data file
-    df = 
+    df = spark.read.json(log_data)
     
     # filter by actions for song plays
-    df = 
+    df = df.filter(df.page == 'NextSong')
 
     # extract columns for users table    
-    artists_table = 
+    users_table = df.select(col("userId").alias("user_id"), col("firstName").alias("first_name"), col("lastName").alias("last_name"), "gender", "level").dropDuplicates()
     
     # write users table to parquet files
-    artists_table
-
-    # create timestamp column from original timestamp column
-    get_timestamp = udf()
-    df = 
+    users_table.write.mode('overwrite').parquet(output_data + 'users_table/')
     
     # create datetime column from original timestamp column
-    get_datetime = udf()
-    df = 
+    get_datetime = udf(lambda x: datetime.fromtimestamp(x / 1000.0))  # refer: Lesson 2 - from Data Wrangling with Spark Exercise '4_data_wrangling' - Calculating Statistics by Hour
+    df = df.withColumn("start_time", get_datetime(df.ts))
     
     # extract columns to create time table
-    time_table = 
+    time_table = df.withColumn("hour",hour("start_time"))\
+                    .withColumn("day",dayofmonth("start_time"))\
+                    .withColumn("week",weekofyear("start_time"))\
+                    .withColumn("month",month("start_time"))\
+                    .withColumn("year",year("start_time"))\
+                    .withColumn("weekday",weekday("start_time"))\
+                    .select("start_time","hour", "day", "week", "month", "year", "weekday").dropDuplicates()   # refer: https://knowledge.udacity.com/questions/806719
     
     # write time table to parquet files partitioned by year and month
-    time_table
-
+    time_table.write.mode('overwrite').partitionBy("year", "month").parquet(output_data + 'time_table/')
+    
     # read in song data to use for songplays table
-    song_df = 
+    song_df = spark.read.parquet(output_data + 'songs_table/')
 
     # extract columns from joined song and log datasets to create songplays table 
-    songplays_table = 
+    songplays_table = df.join(song_df, song_df.title == df.song, how='inner').withColumn("songplay_id", monotonically_increasing_id()) \
+        .select(df.start_time, \
+            col("userId").alias("user_id"), \
+                "level", \
+                "song_id", \
+                "artist_id", \
+                col("sessionId").alias("session_id"), \
+                "location", \
+                col("userAgent").alias("user_agent") \
+            )       # refer https://knowledge.udacity.com/questions/270371
 
     # write songplays table to parquet files partitioned by year and month
-    songplays_table
+    songplays_table.drop_duplicates().write.mode('overwrite').partitionBy("year", "month").parquet(output_data+'songplays_table/')
 
 
 def main():
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
-    output_data = ""
+    output_data = "s3a://{}/".format(S3_BUCKET_OUTPUT)
     
+    t0 = time()
     process_song_data(spark, input_data, output_data)    
     process_log_data(spark, input_data, output_data)
+
+    print('etl done in: {0:.2f} sec(s)\n'.format(time()-t0))
 
 
 if __name__ == "__main__":
